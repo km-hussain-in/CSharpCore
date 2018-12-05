01c32a764283c24a217ed74eefa3b1f92f4f6d6d
km-hussain-in <km.hussain.in@gmail.com>
2018-12-05 12:13:14 -0500

    samples

---
+++ Applications/ModelBindingTest/Models/Visitor.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingTest.Models
+{
+	public class Visitor
+	{
+		public int Id {get; set;}
+
+		public string Name {get; set;}
+
+		[Display(Name = "Visit Count")]
+		public int Frequency {get; set;}
+
+
+		[Display(Name = "Last Visit")]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+		public DateTime Recent {get; set;}		
+
+		public Visitor()
+		{
+			this.Frequency = 1;
+			this.Recent = DateTime.Now;
+		}
+
+		internal void NewVisit()
+		{
+			this.Frequency += 1;
+			this.Recent = DateTime.Now;
+		}
+	}
+}
+ 
+
+
---
+++ Applications/ModelBindingTest/README.md
@@ -0,0 +1,11 @@
+<h4>MVC web-application data model</h4>
+
+<pre>
+dotnet new web
+mkdir Models
+mkdir Controllers
+mkdir Views
+dotnet build
+dotnet run
+</pre>
+
