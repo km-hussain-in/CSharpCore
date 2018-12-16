9f78458d64304168c944f22b3d806c65d5cb6ab3
km-hussain-in <km.hussain.in@gmail.com>
2018-12-15 19:31:54 -0500

    samples

--- Applications/MiddlewareTest/Startup.cs
+++ Applications/MiddlewareTest/Startup.cs
@@ -1,10 +1,17 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MiddlewareTest
 {
 	public class Startup
 	{
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCounter();
+		}
+
 		public void Configure(IApplicationBuilder app)
 		{
 			app.UseCounter();
