dac18a15daf1aeecf620406e186363bd2f08b9c1
km-hussain-in <km.hussain.in@gmail.com>
2019-03-24 15:26:38 -0400

    Latest

--- AspNet/WebHostDemo/DemoApp/Program.cs
+++ AspNet/WebHostDemo/DemoApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
@@ -9,33 +10,74 @@ using Microsoft.AspNetCore.Routing;
 
 namespace DemoApp
 {
+    class Counter
+    {
+        IDictionary<string, int> store = new Dictionary<string, int>();
+
+        public int GetNextCount(string id)
+        {
+            lock(store)
+            {
+                int count;
+                store.TryGetValue(id, out count);
+                store[id] = ++count;
+                return count;
+            }
+        }
+    }
+
+    class Counting
+    {
+        private Counter _counter;
+        private RequestDelegate _next;
+
+        public Counting(Counter counter, RequestDelegate next)
+        {
+            _counter = counter;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Items["VisitCount"] = _counter.GetNextCount(context.Request.Path.Value);
+            await _next.Invoke(context);
+        }
+    }
+
     class Setup
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCounter();
-            //services.AddCounter(options => options.Increment = 2);
+            services.AddSingleton<Counter>();
             services.AddRouting();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseCounter();
-            //app.Run(Greeter);
-            app.UseRouter(route => route.MapGet("Greet/{name=World}", Greeter));
+            app.UseMiddleware<Counting>();
+            //app.Run(Clock);
+            app.UseRouter(routes => 
+            {
+                routes.MapGet("Time", Clock);
+                routes.MapGet("Greet/{name=World}", Greeter);
+            });
+        }
+
+        private async Task Clock(HttpContext context)
+        {
+            await context.Response.WriteAsync(DateTime.Now.ToString());
         }
 
         private async Task Greeter(HttpContext context)
         {
-            var visits = context.GetVisitCount();
-            var visitor = context.GetRouteValue("name");//context.GetRouteData().Values["name"];
+            var visits = context.Items["VisitCount"];
+            var visitor = context.GetRouteValue("name");
             await context.Response.WriteAsync
             ($@"
                 <html>
                     <head><title>WebHostTest</title></head>
                     <body>
                         <h1>Hello {visitor}</h1>
-                        <p>Current time is {DateTime.Now}</p>
                         <p>Number of visits is {visits}</p>
                     </body>
                 </html>
